264b34a1b1c77cf6be494a67ccaf886b8329dde3
frobee <frobi.webdev@outlook.com>
2023-07-14 18:27:03 +0930

    set-up-initial-logic-for-slider

--- script.js
+++ script.js
@@ -1,11 +1,68 @@
 const imageSlider = () => {
-  function activateSlides() {
+  function slideLeft() {
+    const slideContainer = document.querySelector('.slide-container');
+    const styles = window.getComputedStyle(slideContainer);
+    let horizontalOffset = styles.getPropertyValue('right');
+    horizontalOffset = parseInt(horizontalOffset, 10);
+
+    if (horizontalOffset >= -600) {
+      horizontalOffset -= 200;
+      horizontalOffset = `${horizontalOffset}px`;
+    }
+
+    slideContainer.style.right = horizontalOffset;
+  }
+
+  function slideRight() {
+    const slideContainer = document.querySelector('.slide-container');
+    const styles = window.getComputedStyle(slideContainer);
+    let horizontalOffset = styles.getPropertyValue('right');
+    horizontalOffset = parseInt(horizontalOffset, 10);
+
+    if (horizontalOffset <= -200) {
+      horizontalOffset += 200;
+      horizontalOffset = `${horizontalOffset}px`;
+    }
+
+    slideContainer.style.right = horizontalOffset;
+  }
+
+  function viewCenter() {
 
   }
 
+  function viewCenterRight() {
+
+  }
+
+  function viewCenterLeft() {
+
+  }
+
+  function viewFarRight() {
+
+  }
+
+  function viewFarLeft() {
+
+  }
+
+  function activateSlideButtons() {
+    const rightButton = document.querySelector('.right-button');
+    const leftButton = document.querySelector('.left-button');
+
+    rightButton.addEventListener('click', () => {
+      slideRight();
+    });
+
+    leftButton.addEventListener('click', () => {
+      slideLeft();
+    });
+  }
+
   return {
-    activateSlides,
+    activateSlideButtons,
   };
 };
 
-imageSlider().activateSlides();
+imageSlider().activateSlideButtons();
